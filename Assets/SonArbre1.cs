411a2b64c621393d6f2c96c8e89109e80b2ecb31
GaPiko-dev <gael.courrege@lecole-ldlc.com>
2019-12-11 14:42:30 +0100

    Gaël - Son au niveau de la collision

---
+++ Assets/SonArbre1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+ 
+
+public class SonArbre1 : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Projectile"))
+        {
+            GetComponent<AudioSource> ().Play ();
+            Destroy(other.gameObject);
+        }
+    }
+}
